a73d3f839932914277374e64df57055491d16f6a
dorim1209 <dorim1209@gmail.com>
2024-08-20 23:02:45 +0900

    feat: 5장 게시글 읽기

--- src/main/java/com/example/firstproject/controller/MemberController.java
+++ src/main/java/com/example/firstproject/controller/MemberController.java
@@ -6,8 +6,12 @@ import com.example.firstproject.repository.MemberRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.ArrayList;
 
 @Slf4j
 @Controller
@@ -32,5 +36,20 @@ public class MemberController {
         log.info(saved.toString());
 
         return "";
+    }
+
+    @GetMapping("/members/{id}")
+    public String show(@PathVariable Long id, Model model){
+        Member member = memberRepository.findById(id).orElse(null);
+        model.addAttribute("member", member);
+        return "members/show";
+
+    }
+
+    @GetMapping("/members")
+    public String index(Model model){
+        ArrayList<Member> memberEntityArray = memberRepository.findAll();
+        model.addAttribute("memberList", memberEntityArray);
+        return "members/index";
     }
 }
